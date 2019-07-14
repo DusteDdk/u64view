aa8e9bd34c24be7b802e15d19f9f7dde43a0b699
DusteD <dusted@dusted.dk>
2019-07-14 19:45:58 +0200

    Added binaries.

--- release.sh
+++ release.sh
@@ -15,7 +15,7 @@ mkdir -p $win/docs
 cp windows/*.txt $win/docs/
 cp LICENSE $win/docs/
 cp windows/README.md  $win/
-cp windows/*.dll $win/
+# cp windows/*.dll $win/
 cp windows/*.exe $win/
 
 zip -r -9 $win.zip $win
